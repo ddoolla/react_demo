35e2f28d0209cc26b538c2d3df7fb9a3b257ce6c
jh_lee <juhyean8723@gmail.com>
2025-05-12 17:48:00 +0900

    react-hook-form-mui 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,6 @@
 import {useNavigate} from "react-router-dom";
+import {FormContainer, FormErrorProvider, PasswordElement, TextFieldElement} from "react-hook-form-mui";
+import { Button } from "@mui/material";
 
 const Home = () => {
     const nav = useNavigate();
@@ -12,6 +14,27 @@ const Home = () => {
             <button onClick={() => nav("/mui/test")}>
                 MUI test
             </button>
+            <FormContainer
+                defaultValues={{name: ''}}
+                onSuccess={data => console.log(data)}
+            >
+                <TextFieldElement name="name" label="Name" required/>
+                <FormErrorProvider onError={error => {
+                    console.log('you can hook your own error message', error);
+                    if (error.type === 'required') {
+                        return 'ㅇㅇㅇㅇㅇㅇ';
+                    }
+                    if (error?.type === 'pattern') return '올바른 이메일 형식을 입력해주세요.';
+                    return error?.message;
+                }}>
+                        <TextFieldElement name={'name'} label={'Name'} required variant={'outlined'} margin={'dense'} />
+                        <TextFieldElement name={'email'} type="email" label={'Email'} required variant={'outlined'} margin={'dense'} />
+                    <PasswordElement margin={'dense'} label={'Password'} required name={'password'} />
+                </FormErrorProvider>
+                <Button type={'submit'} color={'primary'}>
+                    Submit
+                </Button>
+            </FormContainer>
         </div>
     )
 }
